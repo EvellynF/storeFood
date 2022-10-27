7513abd62ebd23951b1bd2cf28c97321b9e006e4
Antonio Mateus <antoniomso2014@outlook.com>
2022-10-27 11:40:17 -0300

    creating a project

---
+++ com.estacio/src/test/java/storefood/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package storefood.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
